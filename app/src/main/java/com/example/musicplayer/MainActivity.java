3cfecde4e3375ff735340912a724ff614f9f6071
Singroff <caleb.epps@yahoo.com>
2021-10-01 16:42:03 -0400

    Added basic functionality for selecting ONE audio file. Nothing will happen when you select it and hit done, BUUUUUUUUUT it doesn't crash so 10/10

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+// Method that is called when our select a folder button is pressed. It will open a file browser on the user's phone.
+    public void selectFolderBtn(View view) {
+        // Create an intent. See the Android docs for more info.
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Here we're saying to ignore files that aren't local. For example, Google Drive, One Drive, etc.
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        // this is setting the type of file we want the user to be able to select (NEED TO BE ABLE TO SELECT FOLDER)
+        intent.setType("audio/*");
+        // Start the activity.
+        startActivity(intent);
+
     }
 }
