e98b9d3a89f723c4fb0618f7c0d80d972c746395
Singroff <caleb.epps@yahoo.com>
2021-10-01 20:21:11 -0400

    Selecting file works, errors when attempting to play it. Admitting defeat for now.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,30 +1,74 @@
 package com.example.musicplayer;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
+    // onCreate Method for doing... Everything?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Created the Launcher Variable here for our file selection activity
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        String path = uri.toString();
+                        Log.e("THE FILE PATH",path);
+
+                        // Try Catch Statement to try and initialize music player
+                        try {
+                            MediaPlayer mp = new MediaPlayer();
+                            mp.setDataSource(path);
+                            mp.start();
+                        } catch (IllegalArgumentException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (SecurityException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (IllegalStateException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+// Just grabbing our button, should probably move to top.
+        Button selectButton = findViewById(R.id.button);
+// Sets our button listener
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // This is launching the activity to select a file
+                mGetContent.launch("audio/*");
+            }
+        });
     }
 
-// Method that is called when our select a folder button is pressed. It will open a file browser on the user's phone.
-    public void selectFolderBtn(View view) {
-        // Create an intent. See the Android docs for more info.
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        // Here we're saying to ignore files that aren't local. For example, Google Drive, One Drive, etc.
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-        // this is setting the type of file we want the user to be able to select (NEED TO BE ABLE TO SELECT FOLDER)
-        intent.setType("audio/*");
-        // Start the activity.
-        startActivity(intent);
 
-    }
 }
